423ca7d6cfa172ba5258f7e466c2227bfdc4160f
Darlington02 <darlingtonnnam@gmail.com>
2023-10-17 15:35:48 +0100

    chore: bump starknet-react to v2.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import StarknetProvider from "./components/starknet-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+        <StarknetProvider>
+          {children}
+        </StarknetProvider>
+        </body>
+      </html>
   );
 }
